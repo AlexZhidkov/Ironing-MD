f1ace338f56849627041e8d708b67b1ca403aee9
Alexander Zhidkov <azhidkov@gmail.com>
2016-03-01 22:43:33 +0800

    Material order form working

--- src/components/order-form/order-form.ts
+++ src/components/order-form/order-form.ts
@@ -1,34 +1,44 @@
-import { Component } from 'angular2/core';  
-import {  
-  FORM_DIRECTIVES,  
-  FormBuilder,  
-  ControlGroup  
-} from 'angular2/common';
+import {View, Component} from 'angular2/core';
+import {MdPatternValidator, MdMinValueValidator, MdNumberRequiredValidator, MdMaxValueValidator, MATERIAL_DIRECTIVES} from 'ng2-material/all';
+import {FORM_DIRECTIVES, Validators, FormBuilder, ControlGroup} from 'angular2/common';
 
-const styles: string = require('./order-form.scss');
-const template: string = require('./order-form-classic.html');
+const template: string = require('./order-form.html');
+//const styles: string = require('./order-form.scss');
 
-@Component({  
-  selector: 'order-form',  
-  directives: [FORM_DIRECTIVES],  
-  styles: [styles],
+@Component({selector: 'order-form'})
+@View({
+  directives: [MATERIAL_DIRECTIVES, FORM_DIRECTIVES],
+  //styles: [styles],
   template
-})  
 
-export class OrderForm {  
-  myForm: ControlGroup;
-
-  constructor(fb: FormBuilder) {  
-    this.myForm = fb.group({  
-      'name': [],
-      'email': [],
-      'phone': [],
-      'subject': [],
-      'message': []
-    });  
-  }
-
-  onSubmit(value: string): void {  
-    console.log('you submitted value: ', value);  
+})
+export class OrderForm {
+  projectForm: ControlGroup;
+  model = {
+    clientName: '',
+    clientEmail: '',
+    description: 'Nuclear Missile Defense System',
+    rate: 500
+  };
+  constructor(fb: FormBuilder) {
+    this.projectForm = fb.group({
+      'clientName': ['', Validators.required],
+      'description': ['', Validators.compose([
+        Validators.required,
+        Validators.maxLength(30)
+      ])],
+      'clientEmail': ['', Validators.compose([
+        MdPatternValidator.inline('^.+@.+\..+$'),
+        Validators.required,
+        Validators.minLength(10),
+        Validators.maxLength(100)
+      ])],
+      'rate': ['', Validators.compose([
+        MdNumberRequiredValidator.inline(),
+        MdPatternValidator.inline('^1234$'),
+        MdMinValueValidator.inline(800),
+        MdMaxValueValidator.inline(4999)
+      ])]
+    });
   }
 }
