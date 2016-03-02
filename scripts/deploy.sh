ec3a1980649668c790f4cf1249cd8e0bb8216706
Alex Zhidkov <AlexZhidkov@users.noreply.github.com>
2016-03-02 17:52:32 +0800

    token WIP

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,5 +2,5 @@
 set -ev
 
 if [[ "$TRAVIS_BRANCH" == "master" && "$TRAVIS_PULL_REQUEST" == "false" ]]; then
-  firebase deploy --token "$FIREBASE_TOKEN"
+  firebase deploy --token "-JSYOK9la68ygdvyHYuj|9b0d42209f350d795d382acc4d943579"
 fi
